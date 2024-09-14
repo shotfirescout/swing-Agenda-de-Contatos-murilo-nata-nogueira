42ce69c85fb006f3e173ee3e02806110faf8f94b
murilonat nogueira <murilonata376@gmail.com>
2024-09-14 14:23:01 -0300

    adicionando os arquivos do projeto

---
+++ src/main/java/model/Contato.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author muril
+ */
+public class Contato {
+    private String nome, email, telefone;
+
+    public Contato() {
+    }
+
+    
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " | " + telefone + " | " + email;
+    }
+    
+    
+}
